ead0132012d2ceea06680e9499d36a38f5b47437
cashcoder88 <cashglobe@gmail.com>
2019-06-24 14:08:48 -0600

    Doing migrations and seeds

---
+++ configSecrets.js/secret.js
@@ -0,0 +1,3 @@
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || 'dhgj^!*&#63(!gdTR^%9q8sdh!!!89d7'
+}
--- server.js
+++ server.js
@@ -12,4 +12,6 @@ server.get('/', (req, res) => {
     res.status(200).json('Server is ALIVE!')
 });
 
+server.use('/auth', authRouter)
+
 module.exports = server;
