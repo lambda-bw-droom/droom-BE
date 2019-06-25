9782216f0c826b6d9ec02fcca9cb1d6ef5368461
cashcoder88 <cashglobe@gmail.com>
2019-06-25 11:46:55 -0600

    Migration now populated with all data

--- data/migrations/20190624140935_droom_migration.js
+++ data/migrations/20190624140935_droom_migration.js
@@ -4,9 +4,64 @@ exports.up = function(knex, Promise) {
         tbl.string('username').notNullable().unique();
         tbl.string('password').notNullable();
         tbl.boolean('is_employer').notNullable().defaultTo('false');
-        
+        tbl.string('first_name').notNullable()
+        tbl.string('last_name').notNullable()
+        tbl.string('email').notNullable().unique()
+        tbl.string('about');
+        tbl.string('phone');
+        tbl.string('linkedIn');
+        tbl.string('github');
+        tbl.string('website');
+        tbl.string('location');
+        tbl.string('position');
+        tbl.string('bio');
+        tbl.string('interests');
+        tbl.string('education');
+        tbl.string('skills');
+        tbl.string('portfolio');
+        tbl.string('references');
+        tbl.timestamp('timestamp');
+        // linking niche to user
+        tbl
+        .integer('niche_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('niche')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+
+    .createTable('niche', tbl => {
+        tbl.increments();
+        tbl.string('niche').notNullable();
+    })
+
+    .createTable('jobs', tbl => {
+        tbl.increments();
+        tbl.string('job_title');
+        tbl.string('start_date');
+        tbl.string('job_type');
+        tbl.string('starting_pay');
+        tbl.string('description');
+        tbl.string('responsibilities');
+        tbl.string('required_skills')
+        // linking niche to job
+        tbl
+        .integer('niche_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('niche')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+        tbl.boolean('seen').defaultTo('false')
+
+
     })
 };
+
+
 
 exports.down = function(knex, Promise) {
     return knex.schema.dropTableIfExists('users');
--- datastructure.js
+++ datastructure.js
@@ -1,66 +1,64 @@
+/*
 // Accepted Employer Profile Schema
 ```{
-    name: string,
-    location: string,
-    about: string,
+    name: string, *
+    location: string, *
+    about: string, *
     contact_info: {
-        phone: string,
-        email: string
+        phone: string, *
+        email: string *
     },
     social_media: {
-        facebook: string,
-        linkedIn: string,
-        twitter: string,
-        github: string
+        linkedIn: string,*
+        github: string *
     },
-    website: string
+    website: string *
 }```
 ##### Accepted Seeker Profile Schema
 ```{
-    first_name: string, 
-    last_name: string, 
-    position: string,
-    location: string, 
-    bio: string, 
-    job_type: string,
+    first_name: string, *
+    last_name: string, *
+    position: string, *
+    location: string, *
+    bio: string, *
     contact_info: {
-        phone_number: string,
+        phone_number: string, *
         email: string
-    },
+    }, *
     interests: [array of interests],
-    past_experience: [ array of:
-        {
-            name: string,
-            title: string,
-            description: string
-        }
-    ], 
-    education: [
+    // past_experience: [ array of:
+    //     {
+    //         name: string,
+    //         title: string,
+    //         description: string
+    //     }
+    // ], 
+    education: [ *
         {
             school: string,
             certificate: string
         }
     ],
     skills: [array of skills],
-    references: [ array of:
-        {
-            name: string,
-            relationship: string,
-            phone: string,
-            email: string
-        }
-    ],
+    // references: [ array of:
+    //     {
+    //         name: string,
+    //         relationship: string,
+    //         phone: string,
+    //         email: string
+    //     }
+    // ],
     social_media: {
-        facebook: string,
-        linkedIn: string,
-        twitter: string,
-        github: string
+
+        linkedIn: string, *
+
+        github: string *
     },
-    portfolio: string,
-    resume: string,
-    projects: [array of urls],
+    portfolio: string, *
+    resume: string, ////
+    projects: [array of urls], ///
     niche: integer(references niche id), 
-    seen: [array of job ids that have been seen by user], 
+    => seen: [array of job ids that have been seen by user], <-
     timestamp: string 
 }```
 
@@ -72,22 +70,22 @@
 
 #### Accepted Job Schema
 ```{ 
-    job_title: string, 
-    start_date: string,
-    job_type: string(part-time, full-time, seasonal),
-    pay_type: string, 
-    starting_pay: string,
-    education: string,
-    description: string,
-    responsibilities: [array of responsibilites],
-    required_skills: [array of skills],
-    appliers: [array of seeker user_ids that have said yes],
-    confirmed: [array of seeker user_ids that are confirmed by employer],
+    job_title: string, **
+    start_date: string, **
+    job_type: string(part-time, full-time, seasonal), **
+    pay_type: string, //
+    starting_pay: string, **
+    education: string, //
+    description: string, **
+    responsibilities: string,
+    required_skills: string, **
+    => appliers: [array of seeker user_ids that have said yes],
+    => confirmed: [array of seeker user_ids that are confirmed by employer],
     niche: integer(references niche id), 
     seen: boolean,
 }```
 
-#### Returned Schema for `/jobs/matches/employer`
+ => #### Returned Schema for `/jobs/matches/employer`
 ```{
   job: {
     title: job_title,
@@ -112,3 +110,4 @@
     }
   ]
 }```
+*/
