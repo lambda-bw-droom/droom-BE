29b4be516f9663e0b67e91e46c9531fde41e02dd
cashcoder88 <cashglobe@gmail.com>
2019-06-25 11:50:15 -0600

    Updated downs, added two seeds

--- data/migrations/20190624140935_droom_migration.js
+++ data/migrations/20190624140935_droom_migration.js
@@ -64,5 +64,8 @@ exports.up = function(knex, Promise) {
 
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('users');
+    return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('niche')
+    .dropTableIfExists('jobs');
 };
