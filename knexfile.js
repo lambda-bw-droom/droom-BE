42afcf39f2374ce98862afbb8e60e040a22ddaf3
cashcoder88 <cashglobe@gmail.com>
2019-06-24 18:11:49 -0600

    Production and Testing envs set up

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,37 @@ module.exports = {
   },
 
 
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+
 };
 
 
