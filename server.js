02cbcb91ed3ae211b5d6c79854757ada135b14f5
cashcoder88 <cashglobe@gmail.com>
2019-06-25 16:59:21 -0600

    removed middleware from server

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const cors = require('cors');
 const helmet = require('helmet');
 const authRouter = require('./authentication/authRouter.js')
 const usersRouter = require('./users/usersRouter.js')
-const restrict = require('./authorization')
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-server.use(restrict());
+
 
 server.get('/', (req, res) => {
     res.status(200).json('Server is ALIVE!')
