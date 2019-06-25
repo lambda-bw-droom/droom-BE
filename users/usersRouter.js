725d29b7c490580e61233067dcef901696ea310a
cashcoder88 <cashglobe@gmail.com>
2019-06-25 16:20:56 -0600

    fixed error in usersRouter, server to router

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
     })
 });
 
-server.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const id = req.params.id;
 
     UsersModel.getUserById(id)
