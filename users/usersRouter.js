ec79a89d20a5072f50f00366426e4b5f8740edc7
cashcoder88 <cashglobe@gmail.com>
2019-06-25 19:31:28 -0600

    implemented restrict on GET endpoints

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const UsersModel = require('./usersModel.js');
 const restrict = require('../authorization.js')
 
-router.get('/', (req, res) => {
+router.get('/', restrict, (req, res) => {
     UsersModel.getUsers()
     .then(users => {
         res.status(200).json(users)
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', restrict, (req, res) => {
     const id = req.params.id;
 
     UsersModel.getUserById(id)
