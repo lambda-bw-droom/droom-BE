0058e0a354cd76f747fb671cf38ebc838390be62
cashcoder88 <cashglobe@gmail.com>
2019-06-27 10:30:54 -0600

    updated returned object from login

--- authentication/authRouter.js
+++ authentication/authRouter.js
@@ -41,8 +41,8 @@ router.post('/login', (req, res) => {
 
             res.status(200).json({
                 message: `Welcome ${user.email}!`,
-                userType: user.is_employer,
-                userId: user.id,
+                is_employer: user.is_employer,
+                id: user.id,
                 token
             });
         } else {
